e5043655af18253c27d7a6c4dc4d60595b736b34
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2026-03-28 07:33:57 -0300

    feat: add axios dependency and update Keycloak clientId in auth configuration

--- Financeiro.Web/src/auth.ts
+++ Financeiro.Web/src/auth.ts
@@ -3,7 +3,7 @@ import Keycloak from 'keycloak-js';
 const keycloak = new Keycloak({
   url: 'http://localhost:8080', // URL do seu Docker
   realm: 'Financeiro',
-  clientId: 'Financeiro-realm',
+  clientId: 'financeiro-api2',
 });
 
 export default keycloak;
